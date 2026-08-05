cc1e5935b4edbd703989cb3ec947db68dcca28ec
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2026-08-05 17:01:13 +0200

    feat: configure browserslist-based build targeting, add favicon & webmanifest, and improve layout metadata and styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,26 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import browserslistToEsbuild from 'browserslist-to-esbuild';
+import { browserslistToTargets } from 'lightningcss'
+import browserslist from 'browserslist'
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+    // css: {
+    //   transformer: 'lightningcss',
+    //   lightningcss: {
+    //     targets: browserslistToTargets(browserslist()),
+    //     include: 0,
+    //     drafts: {
+    //       customMedia: true,
+    //     },
+    //   }
+    // },
+    build: {
+      target: browserslistToEsbuild(),
+    }
+  },
 });
